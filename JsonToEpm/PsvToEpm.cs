82d85a290a8768134816434037431a3155e9617d
Still Hsu <5843208+Still34@users.noreply.github.com>
2018-03-12 15:22:47 +0800

    Add project files.

---
+++ JsonToEpm/PsvToEpm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JsonToEpm
+{
+    class PsvToEpm
+    {
+        static Task Main(string[] args) => new PsvToEpm().StartAsync(args);
+
+        public async Task StartAsync(string[] startupPath)
+        {
+            var path = startupPath.FirstOrDefault();
+            Directory.EnumerateFiles(path, "course-info.json");
+        }
+    }
+}
